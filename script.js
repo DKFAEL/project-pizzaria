f07b1fd6aa5b4cccc5cc2d5c8fa6d790c152c8e5
DKFAEL <rafaelnunesemp@hotmail.com>
2023-02-11 00:16:25 -0300

    Finalizando primeira versão do projeto

--- script.js
+++ script.js
@@ -101,7 +101,18 @@ c('.pizzaInfo--addButton').addEventListener('click', () => {
     closeModal();
 });
 
+c('.menu-openner').addEventListener('click', () =>{
+    if(cart.length > 0) {
+        c('aside').style.left = '0';
+    }
+});
+c('.menu-closer').addEventListener('click', ()=>{
+    c('aside').style.left = '100vw';
+})
+
 function UpCart() {
+    c('.menu-openner span').innerHTML = cart.length;
+
     if (cart.length > 0) {
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
@@ -161,5 +172,6 @@ function UpCart() {
 
     } else {
         c('aside').classList.remove('show');
+        c('aside').style.left = '100vw'
     }
 }
